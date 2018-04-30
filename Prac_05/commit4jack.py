4c49c09ae665ac2f132926a177fd56a058ae4a73
Jords <jorda.kusabs@my.jcu.edu.au>
2018-04-30 17:02:01 +1000

    asdf

---
+++ Prac_05/commit4jack.py
@@ -0,0 +1,16 @@
+string = input("Enter a sentence ")
+counts = dict()
+words = string.split()
+
+longest = len(max(words, key=len))
+
+for word in words:
+    if word in counts:
+        counts[word] += 1
+    else:
+        counts[word] = 1
+
+
+for key in sorted(counts):
+    print("{:{}}: {}".format(key, longest, counts[key]))
+
